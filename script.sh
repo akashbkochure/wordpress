160121d9ee9f3a782ec9284c5da2cb7fd77fcf95
Akash Bharat Kochure <115985398+akashbkochure@users.noreply.github.com>
2023-08-17 00:09:20 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,6 @@
+sudo mkdir -p /var/www/www.akashbkochure.com
+cd /var/www/www.akashbkochure.com
+sudo rm -rf wordpress
+sudo git clone https://github.com/akashbkochure/wordpress.git
+sudo systemctl stop nginx
+sudo systemctl start nginx
