28cb30ecd3a027490fd79fe08a4e63f2d1c452b2
Akash Bharat Kochure <115985398+akashbkochure@users.noreply.github.com>
2023-08-17 00:33:19 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,17 @@
-ssh -i ubuntu@52.91.181.6
+#!/bin/bash
 
+# Change to the directory containing the website files
 cd /var/www/www.akashbkochure.com
+
+# Remove existing WordPress installation
 sudo rm -rf wordpress
 
+# Stop and start the Nginx service
 sudo systemctl stop nginx
 sudo systemctl start nginx
+
+# Print a message at the end of the script
+echo "Deployment completed."
+
+# Exit the script
+exit 0
