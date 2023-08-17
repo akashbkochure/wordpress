0990a4f3de33059e832736addb0755bb76660f3b
Akash Bharat Kochure <115985398+akashbkochure@users.noreply.github.com>
2023-08-17 12:45:45 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo rm -rf wordpress
-
 # Change to the directory containing the website files
 cd /var/www/www.akashbkochure.com
 
@@ -21,44 +19,10 @@ sudo sed -i "s/password_here/example_pw/" wp-config.php
 SALT_KEYS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
 sudo sed -i "/AUTH_KEY/s/put your unique phrase here/$SALT_KEYS/" wp-config.php
 
-# Create Nginx configuration
-sudo tee /etc/nginx/sites-available/www.akashbkochure.com > /dev/null <<EOT
-server {
-    listen 80;
-    server_name www.akashbkochure.com;
-
-    root /var/www/www.akashbkochure.com/wordpress;
-    index index.php;
-
-    location ^~ /.well-known/acme-challenge/ {
-        allow all;
-        root /var/www/www.akashbkochure.com/wordpress;
-    }
-
-    location / {
-        try_files \$uri \$uri/ /index.php?\$args;
-    }
-
-    location ~ \.php$ {
-        include snippets/fastcgi-php.conf;
-        fastcgi_pass unix:/run/php/php8.1-fpm.sock;
-    }
-
-    location ~ /\.ht {
-        deny all;
-    }
-}
-EOT
-
-# Test Nginx configuration
-sudo nginx -t
 
 # Reload Nginx
 sudo systemctl reload nginx
 
-# Stop and start the Nginx service
-sudo systemctl stop nginx
-sudo systemctl start nginx
 
 # Print a message at the end of the script
 echo "Deployment completed."
