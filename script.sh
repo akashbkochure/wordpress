ff058e1acc6aa8ffe0dcf641ff52ffd128fe0904
Akash Bharat Kochure <115985398+akashbkochure@users.noreply.github.com>
2023-08-17 09:44:12 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,6 +11,13 @@ cd wordpress
 # Configure WordPress
 sudo rm -rf wp-config.php
 sudo cp wp-config-sample.php wp-config.php
+sudo sed -i "s/database_name_here/example_db/" wp-config.php
+sudo sed -i "s/username_here/example_user/" wp-config.php
+sudo sed -i "s/password_here/example_pw/" wp-config.php
+
+# Get salt keys and add to wp-config.php
+SALT_KEYS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
+sudo sed -i "/AUTH_KEY/s/put your unique phrase here/$SALT_KEYS/" wp-config.php
 
 # Create Nginx configuration
 sudo tee /etc/nginx/sites-available/www.akashbkochure.com > /dev/null <<EOT
