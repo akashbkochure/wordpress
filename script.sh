ea843b4887114edb18b7a06a06b4d8898b540e78
Akash Bharat Kochure <115985398+akashbkochure@users.noreply.github.com>
2023-08-17 09:35:37 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,6 +6,45 @@ cd /var/www/www.akashbkochure.com
 # Remove existing WordPress installation
 sudo rm -rf wordpress
 sudo git clone https://github.com/akashbkochure/wordpress.git
+cd wordpress
+
+# Configure WordPress
+sudo cp wp-config-sample.php wp-config.php
+
+# Create Nginx configuration
+sudo tee /etc/nginx/sites-available/www.akashbkochure.com > /dev/null <<EOT
+server {
+    listen 80;
+    server_name www.akashbkochure.com;
+
+    root /var/www/www.akashbkochure.com/wordpress;
+    index index.php index.html index.htm;
+
+    location ^~ /.well-known/acme-challenge/ {
+        allow all;
+        root /var/www/www.akashbkochure.com/wordpress;
+    }
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$args;
+    }
+
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/run/php/php8.1-fpm.sock;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}
+EOT
+
+# Test Nginx configuration
+sudo nginx -t
+
+# Reload Nginx
+sudo systemctl reload nginx
 
 # Stop and start the Nginx service
 sudo systemctl stop nginx
