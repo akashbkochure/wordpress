b1bc4281d4ab336242d3b5e7fad2f3f17af1ec70
Akash Bharat Kochure <115985398+akashbkochure@users.noreply.github.com>
2023-08-23 08:37:13 +0530

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -3,15 +3,26 @@
 # Change to the directory containing the website files
 cd /var/www/www.akashbkochure.com
 
-# Pull the latest changes in code
-sudo git pull
+# Remove existing WordPress installation
+sudo rm -rf wordpress
+sudo git clone https://github.com/akashbkochure/wordpress.git
+cd wordpress
+
+# Configure WordPress
+sudo rm -rf wp-config.php
+sudo cp wp-config-sample.php wp-config.php
+sudo sed -i "s/database_name_here/example_db/" wp-config.php
+sudo sed -i "s/username_here/example_user/" wp-config.php
+sudo sed -i "s/password_here/example_pw/" wp-config.php
+
+# Get salt keys and add to wp-config.php
+SALT_KEYS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
+sudo sed -i "/AUTH_KEY/s/put your unique phrase here/$SALT_KEYS/" wp-config.php
+
 
 # Reload Nginx
 sudo systemctl reload nginx
 
-# Renew SSL certificate Test & Renew
-sudo certbot renew --dry-run
-sudo certbot renew
 
 # Print a message at the end of the script
 echo "Deployment completed."
